29f0e71b7085f7e02c7a4c17cb860bc418361368
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-03-21 15:15:05 +0330

    feat: --> Form Component | add form component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeadingComponent } from './heading/heading.component';
+import { FormComponent } from './form/form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeadingComponent],
+  imports: [RouterOutlet, HeadingComponent, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss',
+})
+export class FormComponent {
+  onFileDrop(e: Event) {
+    console.log(e.target);
+  }
+}
