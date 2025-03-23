ce27b5a00d6bb454ac5240751cc6607afc9e9abc
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-03-23 16:17:19 +0330

    feat: --> Form Component | import button component to the form component

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DropComponent } from './drop/drop.component';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [DropComponent],
+  imports: [DropComponent, ButtonComponent],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
 })
