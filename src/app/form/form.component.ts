bc86e3f421cb750bd9952cec1bbe805ca1f9f0bb
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-03-25 17:37:36 +0330

    fix: --> Form Component | transfer drop zone logic the form compnent

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,17 +1,55 @@
 import { Component, viewChild, ViewChild } from '@angular/core';
-import { DropComponent } from './drop/drop.component';
 import { ButtonComponent } from '../button/button.component';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [DropComponent, ButtonComponent, FormsModule],
+  imports: [ButtonComponent, FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
 })
 export class FormComponent {
   private form = viewChild<NgForm>('form');
+  file: File | null = null;
+  imagePreview: string | null = null;
+  draggedOver: boolean = false;
+
+  onFileSelect(e: Event | DragEvent) {
+    const target = e.target as HTMLInputElement;
+
+    if (target.files && target.files.length > 0) {
+      this.file = target.files[0];
+
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        if (reader.result) {
+          this.imagePreview = reader.result as string;
+        }
+      };
+
+      reader.readAsDataURL(this.file);
+
+      // Reset the file input to allow selecting the same file again
+      target.value = '';
+    }
+    this.draggedOver = false; // Reset the dragged-over state
+  }
+
+  onDragOver(e: DragEvent) {
+    e.preventDefault();
+    this.draggedOver = true;
+  }
+
+  onDragLeave(e: DragEvent) {
+    this.draggedOver = false;
+  }
+
+  onRemoveImage() {
+    this.imagePreview = null;
+    this.file = null;
+  }
   onSubmit() {
     console.log(this.form()?.form.value.email);
     console.log('Submitted!');
