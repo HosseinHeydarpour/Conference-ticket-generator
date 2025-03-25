e6d7a8fec4e57fda946e43bc002c5e7319eef2fa
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-03-25 17:16:32 +0330

    feat: --> Form Component | register form with angular form api

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, viewChild, ViewChild } from '@angular/core';
 import { DropComponent } from './drop/drop.component';
 import { ButtonComponent } from '../button/button.component';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [DropComponent, ButtonComponent],
+  imports: [DropComponent, ButtonComponent, FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
 })
-export class FormComponent {}
+export class FormComponent {
+  private form = viewChild<NgForm>('form');
+  onSubmit() {
+    console.log(this.form()?.form.value.email);
+    console.log('Submitted!');
+  }
+}
