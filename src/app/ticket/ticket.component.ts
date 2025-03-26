e0aecbcb6ecc3d41b47cdf453691ee0001fded95
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-03-26 17:53:15 +0330

    feat: --> Ticket Component | add ticket component to the project

--- src/app/ticket/ticket.component.ts
+++ src/app/ticket/ticket.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { TicketService } from './ticket.service';
 
 @Component({
   selector: 'app-ticket',
   standalone: true,
   imports: [],
   templateUrl: './ticket.component.html',
-  styleUrl: './ticket.component.scss'
+  styleUrl: './ticket.component.scss',
 })
 export class TicketComponent {
+  ticketService = inject(TicketService);
 
+  user = this.ticketService.user.asReadonly();
+
+  constructor() {}
 }
