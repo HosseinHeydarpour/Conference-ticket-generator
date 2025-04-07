255a24d15d9e4f18776d37d836beffce73f48381
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-07 12:58:33 +0330

    feat: add greeting message after ticket gets generated

--- src/app/heading/heading.component.ts
+++ src/app/heading/heading.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TicketService } from '../ticket/ticket.service';
 
 @Component({
   selector: 'app-heading',
   standalone: true,
   imports: [],
   templateUrl: './heading.component.html',
-  styleUrl: './heading.component.scss'
+  styleUrl: './heading.component.scss',
 })
 export class HeadingComponent {
+  ticketService = inject(TicketService);
 
+  user = this.ticketService.userInfo!;
+
+  get firstName() {
+    return this.user()?.name.split(' ')[0];
+  }
+
+  get lastName() {
+    return this.user()?.name.split(' ')[1];
+  }
 }
